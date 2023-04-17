793328beec3903ef7db26a9c19906d72d53097cb
Preshfile <preshcharlies2014@gmail.com>
2023-04-17 09:58:50 -0700

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,13 @@
-/*modify the variables inside the function taskBlock 
-so that the variables aren’t overwritten inside the conditional block.*/
-
+/* eslint-disable */
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
-  
-    if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
   }
+
+  return [task, task2];
+}
+
