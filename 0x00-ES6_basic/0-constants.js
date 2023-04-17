e1d15d259dfd1543dea8c3518b1bf4aa8399f151
Preshfile <preshcharlies2014@gmail.com>
2023-04-17 16:26:46 +0100

    const or let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,23 +1,15 @@
-/*
-Modify:
-function taskFirst to instantiate variables using const
-function taskNext to instantiate variables using let
-*/
-
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
-  
-  
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
