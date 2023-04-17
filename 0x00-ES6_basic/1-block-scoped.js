aaf55bf5eff7291342b8d3b15b38b7637cdb3ce7
Preshfile <preshcharlies2014@gmail.com>
2023-04-17 16:42:34 +0100

    Block Scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+/*modify the variables inside the function taskBlock 
+so that the variables aren’t overwritten inside the conditional block.*/
+
+export default function taskBlock(trueOrFalse) {
+    var task = false;
+    var task2 = true;
+  
+    if (trueOrFalse) {
+      const task = true;
+      const task2 = false;
+    }
+  
+    return [task, task2];
+  }
